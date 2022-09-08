39174d2420572c139b8a1a2864ee4ebae709047d
SManeesha99 <it20649012@my.sliit.lk>
2022-09-09 00:09:43 +0530

    register admin

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -5,40 +5,30 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
 require("dotenv").config();
-const cookieParser = require('cookie-parse');
-const fileUpload = require("express-fileupload");
-// const routes = require(".routes");
 
 const PORT = process.env.PORT || 8090;
 
-app.use(express.json())
-app.use(cors())
-app.use(fileUpload({
-    useTempFiles: true
-}))
-
-
+app.use(cors());
+app.use(bodyParser.json());
 
 const URL=process.env.MONGODB_URL;
 
 mongoose.connect(URL,{
   
-    
-    useNewUrlParser:true,
-    useUnifiedTopology:true 
- 
- });
+   useNewUrlParser:true,
+   useUnifiedTopology:true,
 
- const connection = mongoose.connection;
 
- connection.once("open",()=>{
+});
+
+const connection = mongoose.connection;
+
+connection.once("open",()=>{
     console.log("MongoDB Connected");
 });
 
-const StudentRoutes = require('./routes/StudentRegistration');
-app.use(StudentRoutes);
-
-app.use('/admin', require('./routes/AdminRegister'))
+const adminRouter = require("./routes/AdminRegister.js");
+app.use("/admin", adminRouter);
 
 app.listen(PORT,()=>{
     console.log(`Server is up and running on port ${PORT}`);
