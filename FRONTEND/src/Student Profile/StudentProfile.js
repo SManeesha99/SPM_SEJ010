5f03e706e86ed270a981c2d6826b8f37358e224d
Deshan Rajapaksha <it20644062@my.sliit.lk>
2022-10-13 00:42:37 +0530

    StudentProfile_FrontEnd
    StudentProfile_FrontEnd

--- FRONTEND/src/Student Profile/StudentProfile.js
+++ FRONTEND/src/Student Profile/StudentProfile.js
@@ -1,8 +1,12 @@
 // import React from 'react';
+import axios from "axios";
 import Header from "../Header/Header";
 import "./studentprofile.css";
+import swal from "sweetalert";
+import Swal from "sweetalert2";
+import { useParams, useNavigate} from "react-router-dom";
 
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -18,6 +22,51 @@ import {Link} from 'react-router-dom';
 import { FreeMode, Pagination } from "swiper";
 
 export default function StudentProfile() {
+
+  const [input, setInput]=useState({});
+  const id =localStorage.getItem("id");
+  console.log(id);
+  const history = useNavigate();
+  useEffect(()=>{ 
+      const fetchHandler = async()=>{
+          await axios.get(`http://localhost:8090/User/get/${id}`)
+          .then((res)=> res.data )
+          .then((data)=>setInput(data.User));
+      }
+      fetchHandler()
+      .then((data)=>setInput(data.User));
+  },[id])
+
+  const navigate = useNavigate();
+
+  function onDelete (id) {
+    axios.delete(`http://localhost:8090/User/delete/${id}`).then((res)=>{
+        Swal.fire({
+            title: 'Are you sure ?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire(
+                'Deleted!',
+                'Your Profile has been deleted.',
+                navigate('/')
+        
+              )
+            }
+          })
+       
+        
+        this.useEffect();
+
+       
+    });
+};
+
   return (
     <div>
         <Header/>
@@ -28,14 +77,14 @@ export default function StudentProfile() {
 
                     </div>
                     <div className='nameNdate'>
-                        <h2>Student Name</h2>
-                        <p>2020.05.23</p>
+                        <h2>{input.firstName} {input.lastName}</h2>
+                        <p>{input.registerAt}</p>
                     </div>
                 
                 </div>
                 <div class="col-sm-4 rights">
-                    <button className='editprofile'><Link className="nav-link active" aria-current="page" to="/editProfile">Edit Profile</Link></button>
-                    <button className='dltprofile'>Delete Profile</button>
+                    <button className='editprofile'><Link className="nav-link active" aria-current="page" to="/editProfileS">Edit Profile</Link></button>
+                    <button className='dltprofile' onClick={()=>onDelete(input._id)}>Delete Profile</button>
                     
                 </div>
             </div>
